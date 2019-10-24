8b298143822141c86b639a1b45235a4e522d7b76
Emanuele Sanna <emanuele.sanna.81@gmail.com>
2019-10-24 09:25:05 +0200

    UPDATE FEATUREUSERMANAGMENT.SQL With proper identation

--- FeatureUserManagment.sql
+++ FeatureUserManagment.sql
@@ -1,43 +1,27 @@
 CREATE TABLE QUESTIONARI_USERMANAGEMENT_ACCESS
-
-
 (
-  ID smallint Primary key,
-
-
-  Username char(40) not null,
-  Password char(40) not null,
-  
-
-Email char(60) not null,
-
-
-  ID_Anagraphic smallint default null,
-  
-
-ID_Permission smallint not null
+	ID smallint Primary key,
+	Username varchar(255) not null,
+	Password varchar(255) not null,
+	Email varchar(255) not null,
+	ID_Anagraphic smallint default null,
+	ID_Permission smallint not null
 );
 
 CREATE TABLE QUESTIONARI_USERMANAGEMENT_ANAGRAPHIC
-
-
 (
-  ID smallint Primary key,
-  Name char(60) default null,
-  Surname char(60) default null,
-  Birth_date char(30) default null,
-  Telephone char(20) default null,
-  City char(40) default null,
-  Birth_place CHAR(40) default null,
-  Sex CHAR(10) default null
+	ID smallint Primary key,
+	Name varchar(255) default null,
+	Surname varchar(255) default null,
+	Birth_date varchar(255) default null,
+	Telephone varchar(255) default null,
+	City varchar(255) default null,
+	Birth_place varchar(255) default null,
+	Gender varchar(255) default null
 );
 
 CREATE TABLE QUESTIONARI_USERMANAGEMENT_PERMISSION
-
-
 (
-  ID smallint Primary key,
-  
-
-Level CHAR(20) not null
+	ID smallint Primary key,
+	Level varchar(255) not null
 );
